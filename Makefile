60f5f0984a6400b17d177c9d963703286d0b4c12
Oliver Stein <oliver-dot-stein@hotmail.com>
2014-10-07 22:56:16 +0200

    Redigerat Makefile lite, gjort klart myListModule.c och fixat tillhörande h-fil

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
+$FLAGS=-std=c99 -ggdb -Wall
 
-resplan: resplan.c
-	gcc -std=c99 -ggdb -Wall -o resplan resplan.c
 
+resplan: resplan.c myListModule.o
+	gcc $(FLAGS) myListModule.o -o resplan resplan.c
+
+myListModule.o: myListModule.c
+	gcc $(FLAGS) -c myListModule.c
 run: 
 	./resplan
 
--- myListModule.h
+++ myListModule.h
@@ -1,15 +1,26 @@
 #ifndef MYLISTMODULE_H
 #define MYLISTMODULE_H
 
+/*
+  A simple datastructure of a generic list with
+  a first and last element, can be further
+  developed with append and prepend functionality.
+  Uses void pointers for data to handle generic
+  list data
+  (C) Oliver Stein 2014
+  Use at own risk! ;)
+ */
+
 typedef struct list *List;
+typedef struct listNode *ListNode;
 
 List createList();
-List addNode();
-List deleteNode();
-void deleteList();
-void* getval(); 
-void* getkey();
-void setVal();
-void setKey(); 
-
+void addNode(List list, void *data);
+ListNode findNode(List list, void* searchData, int *found);
+void deleteNode(List list, ListNode target);
+void deleteList(List list);
+void* getData(ListNode node); 
+void setData(ListNode node, void* newData);
+void clearNode(ListNode node);
+ListNode prevNode(List list, ListNode searchNode);
 #endif
